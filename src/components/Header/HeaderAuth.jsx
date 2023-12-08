68b721fbfcd731bbf37223b0bcf3be534b91f9a4
Wysted <vladimirbesser@gmail.com>
2023-12-08 17:31:11 -0300

    [Fix] Se añadio header correcto y cambio de titulo

--- src/components/Header/HeaderAuth.jsx
+++ src/components/Header/HeaderAuth.jsx
@@ -1,24 +1,46 @@
-import { FaSuitcase, FaQuestionCircle } from "react-icons/fa";
-import Logo from "../Logo/index"; // Importa componentes desde archivos separados
-import NavItem from "./NavItem";
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./Header.css";
-
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "wouter";
 const HeaderAuth = () => {
     return (
-        <header className="header">
-            <div className="left-section">
-                <Logo />
-                <NavItem
-                    icon={<FaSuitcase />}
-                    text="Paquetes"
+        <Navbar
+            expand="md"
+            className="navbar-lighter"
+            style={{ background: "#023047" }}
+        >
+            <div className="container-xl">
+                <Link href="/">
+                    <img
+                        src="/logo.png"
+                        alt="logo"
+                        className="logo_header"
+                    />
+                </Link>
+                <Navbar.Toggle
+                    aria-controls="menu"
+                    className="btn-colapse"
                 />
-                <NavItem
-                    icon={<FaQuestionCircle />}
-                    text="Ayuda"
-                />
+                <Navbar.Collapse id="menu">
+                    <Nav className="me-auto justify-content-center">
+                        <Link
+                            className="link-izq d-flex justify-content-center"
+                            href="/nosotros"
+                        >
+                            <i className="bi bi-info-circle-fill"></i> Nosotros
+                        </Link>
+                        <Link
+                            className="link-izq d-flex justify-content-center"
+                            href="/faq"
+                        >
+                            <i className="bi bi-question-circle-fill"></i> FAQ
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
             </div>
-        </header>
+        </Navbar>
     );
 };
 export default HeaderAuth;
